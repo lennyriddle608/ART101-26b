8f543ed2a55c81e07438776b8b383b002443dfd5
lennyriddle608 <lennyriddle608@gmail.com>
2026-05-21 00:41:36 -0700

    buh

---
+++ environ/env05.js
@@ -0,0 +1,188 @@
+moods = ["tillhappy", "lettinggo", "ermactually", "tillleaving", "iknewit"]// track counts
+let hangoutCount = {
+    tillhappy: 0,
+    lettinggo: 0,
+    ermactually: 0,
+    tillleaving: 0,
+    iknewit: 0
+};
+
+// show image
+function showMoodsImage(moodsName) {
+    // hide all images first
+    $("ivantill").hide();
+    // show only the selected one
+    $("body").append("<img class='ivantill' src='" + moodsName + ".jpg'>");
+}
+
+// hide all images
+$("ivantill").hide();
+
+// mood clicks
+$("#tillhappy").click(function () {
+    console.log("tillhappy clicked");
+    showMoodsImage("tillhappy");
+    moodsClick("tillhappy");
+});
+
+$("#lettinggo").click(function () {
+    console.log("lettinggo clicked");
+    showMoodsImage("lettinggo");
+    moodsClick("lettinggo");
+});
+
+$("#ermactually").click(function () {
+    console.log("ermactually clicked");
+    showMoodsImage("ermactually");
+    moodsClick("ermactually");
+});
+
+$("#tillleaving").click(function () {
+    console.log("tillleaving clicked");
+    showMoodsImage("tillleaving");
+    moodsClick("tillleaving");
+});
+
+$("#iknewit").click(function () {
+    console.log("iknewit clicked");
+    showMoodsImage("iknewit");
+    moodsClick("iknewit");
+});
+
+function askNumber(whatNumber) {
+    let userNumber = prompt("Enter an option:");
+    switch (userNumber) {
+        case "One":
+            $("#output").html("<img src='oneshot.png' alt='One!'>");
+            break;
+        case "Two":
+            $("#output").html("<img src='twoshot.png' alt='Two!'>");
+            break;
+        case "Three":
+            $("#output").html("<img src='threeshot.png' alt='Three!'>");
+            break;
+        case "???":
+            $("#output").html("<img src='broimdead.png' alt='Bro I'm Dead!'>");
+            break;
+        case "...":
+            $("#output").html("<img src='dead.png' alt='Dead!'>");
+            break;
+}
+
+let following = false;
+
+$("#creature").click(function () {
+  $(this).toggleClass("transformed");
+  $("#status").text("The creature changed shape.");
+});
+
+$("#creature").hover(
+  function () {
+    $("#thought").stop(true, true).slideDown(300);
+    $("#status").text("The creature revealed a thought.");
+  },
+  function () {
+    $("#thought").stop(true, true).slideUp(300);
+    $("#status").text("The thought disappeared.");
+  }
+);
+
+$(document).keydown(function (event) {
+  if (event.key === " " || event.code === "Space") {
+    event.preventDefault();
+
+    following = !following;
+    $("#creature").toggleClass("following");
+
+    if (following === true) {
+      $("#status").text("The creature is following your cursor.");
+    } else {
+      $("#status").text("The creature stopped following you.");
+    }
+  }
+});
+
+$(document).mousemove(function (event) {
+  if (following === true) {
+    $("#creature").css({
+      left: event.pageX - $("#scene").offset().left + 30,
+      top: event.pageY - $("#scene").offset().top + 30
+    });
+  }
+});
+
+
+    // if (userNumber == 'One') {
+    //     $("#output").html("<img src='oneshot.png' alt='One!'>");
+    // }
+    // if (userNumber == 'Two') {
+    //     $("#output").html("<img src='twoshot.png' alt='Two!'>");
+    // }
+    // if (userNumber == 'Three') {
+    //     $("#output").html("<img src='threeshot.png' alt='Three!'>");
+    // }
+    // if (userNumber == '???') {
+    //     $("#output").html("<img src='broimdead.png' alt='Bro I'm Dead!'>");
+    // }
+    // if (userNumber == '...') {
+    //     $("#output").html("<img src='dead.png' alt='Dead!'>");
+    // }
+    // else {
+    //     $("#output").html("<img src='dead.png' alt='Dead!'>");
+    // }
+}
+
+$("#good-button").click(function () {
+    askNumber("One", "Two", "Three", "???", "...");
+});
+
+
+// ivan = ["ivantillbabies", "oneshot", "twoshot", "threeshot", "broimdead", "dead"]// track counts
+// let shotsCount = {
+// ivantillbabies: 0,
+// oneshot: 0,
+// twoshot: 0,
+// threeshot: 0
+// broimdead: 0,
+// dead: 0
+// };
+//      function ivanClick(ivanName) {
+//     // increase count
+//     shotsCount[ivanName]++;
+
+//     console.log(ivanName + " clicked");
+//     console.log("Shots with " + ivanName + ": " + shotsCount[ivanName]);
+
+//     // check if hit 5 hangouts
+//     if (clickCount[ivanName] === 5) {
+//     function showMoodsImage(moodsName)
+//     let otherFriends = ivan.filter(i => i !== ivanName);
+
+//     let suggestion = otherFriends[0];
+//     let minCount = shotsCount[suggestion];
+
+//     for (let i = 1; i < otherIvan.length; i++) {
+//         let i = otherIvan[i];
+//         if (hangoutCount[i] < minCount) {
+//             minCount = hangoutCount[i];
+//             suggestion = i;
+//         }
+//     }
+
+//     $("#suggestion-box").html(
+//         "<p>You've hung out with " + friendName + " 5 times. Try hanging out with <b>" 
+//         + suggestion + "</b> (" + minCount + " hangouts).</p>"
+//     );
+// }
+
+//     // show image
+//     showivanImage(ivanName);
+
+//     // update count on page
+//     $("#count-" + ivanName).remove();
+
+//     $("#" + ivanName).append(
+//         "<p id='count-" + ivanName + "'>Shots: " + shotsCount[ivanName] + "</p>"
+//     );
+// }
+
